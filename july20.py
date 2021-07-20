cea9c6a732cc27edd9412f5d690d5a099af0cebb
Jon Mockbee <mockbee1000@gmail.com>
2021-07-20 10:28:00 -0400

    elif statement

--- july20.py
+++ july20.py
@@ -1,8 +1,18 @@
-str_input1 = input("Enter your favorite plant: ")
+str_input1 = input("Enter your favorite plant:  ")
 str2 = ("Spathiphyllum")
+str3 = ("spathiphyllum")
+str4 = ("SPATHIPHYLLUM")
 
 #comparing by ==
 if str_input1 == str2:
     print ("Yes - Spathiphyllum is the best plant ever")
+
+elif str_input1 == str3:
+    print ("No, I want a big Spathiphyllum!")
+
+elif str_input1 == str4:
+    print ("No, I want a big Spathiphyllum!")
+
+
 else:
     print ("No, I want a big Spathiphyllum!")

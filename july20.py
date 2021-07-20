9c4d1e34864b31d11d88d44d42f55fc38505b7d6
Jon Mockbee <mockbee1000@gmail.com>
2021-07-20 10:17:07 -0400

    else if strings

---
+++ july20.py
@@ -0,0 +1,8 @@
+str_input1 = input("Enter your favorite plant: ")
+str2 = ("Spathiphyllum")
+
+#comparing by ==
+if str_input1 == str2:
+    print ("Yes - Spathiphyllum is the best plant ever")
+else:
+    print ("No, I want a big Spathiphyllum!")
